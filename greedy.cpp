d0974e45dba6ef9fa6aa6c19a08e47841a4e308d
michal <mp.szmigiel@gmail.com>
2013-04-29 14:11:35 +0300

    Update greedy.cpp

--- greedy.cpp
+++ greedy.cpp
@@ -18,37 +18,9 @@ void greedy(double m, double w[n], double c[n], double x[n]){
 
 }
 int main(){
-    double a1,b1,c1;
-    double a2,b2,c2;
-    double a3,b3,c3;
-    double w[n]={a1,b1,c1}, c[n]={a2,b2,c2}, x[n]={a3,b3,c3};
-    double m;
-    cout<<"Podaj M : ";
-    cin>>m;
-    cout<<"Dla w[i]"<<endl;
-    cout<<"Podaj pierwsza wartosc : ";
-    cin>>a1;
-    cout<<"Podaj druga wartosc : ";
-    cin>>b1;
-    cout<<"Podaj trzecia wartosc : ";
-    cin>>c1;
-    cout<<"\n\n";
-    cout<<"Dla c[i]"<<endl;
-    cout<<"Podaj pierwsza wartosc : ";
-    cin>>a2;
-    cout<<"Podaj druga wartosc : ";
-    cin>>b2;
-    cout<<"Podaj trzecia wartosc : ";
-    cin>>c2;
-    cout<<"\n\n";
-    cout<<"Dla x[i]"<<endl;
-    cout<<"Podaj pierwsza wartosc : ";
-    cin>>a3;
-    cout<<"Podaj druga wartosc : ";
-    cin>>b3;
-    cout<<"Podaj trzecia wartosc : ";
-    cin>>c3;
-    cout<<"\n\n";
+
+    double w[n]={10,12,16}, c[n]={60,70,80}, x[n]={0,0,0};
+    double m = 20;
     greedy(m,w,c,x);
     double p=0;
     for(int i=0 ; i<n; p+=x[i]*c[i], i++){
